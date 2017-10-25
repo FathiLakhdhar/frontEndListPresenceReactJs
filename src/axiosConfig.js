61c0b79a0f9dd501a127625149951ea6f654f005
Fathi Lakhdhar <fathi.lakdhar@yahoo.fr>
2017-10-25 21:47:11 +0100

    axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
-export default function setAuthorizationToken (token) {
+export  function setAuthorizationToken (token) {
     (token) ? axios.defaults.headers.common['Authorization'] = 'Bearer '+token : delete axios.defaults.headers.common['Authorization'];
+}
+
+export function setAxiosBaseUrl(base = 'http://localhost:3030/'){
+    axios.defaults.baseURL = base;
 }
