a3602bf79c3644db784ad7859340eaa9c62ab48b
Fathi Lakhdhar <fathi.lakdhar@yahoo.fr>
2017-10-25 22:16:04 +0100

    change txt Init todos

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,6 +1,7 @@
 var todos = [
-    { id: 0, complited: false, text: 'AZERTY' },
-    { id: 1, complited: false, text: 'QUERY' },
+    { id: 0, complited: false, text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.' },
+    { id: 1, complited: false, text: 'Aliquam tincidunt mauris eu risus.' },
+    { id: 2, complited: false, text: 'Vestibulum auctor dapibus neque.' },
 ];
 export default function (state = todos, action) {
     const { type, payload, id, toggleAll } = action;
